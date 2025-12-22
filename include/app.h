c5752536a6cce3e1d1b3dcd6ee9ff347a0454370
NNN706 <150787520+NNN706@users.noreply.github.com>
2025-12-22 18:16:18 +0300

    Add files via upload

--- include/app.h
+++ include/app.h
@@ -7,24 +7,21 @@
 
 // Способы ввода массива (избегаем магических чисел)
 enum class InputMethod {
-    Keyboard = 1,
-    Random = 2,
-    File = 3,
-    Exit = 4
+    Keyboard,   
+    Random,    
+    File,       
+    Exit        
 };
-
-// Класс приложения: отвечает за ввод, вывод и запуск сортировки
 class App {
 public:
-    // Запуск приложения
     int run();
 
 private:
     // Методы ввода/вывода
     std::vector<int> readFromFile(const std::string& path) const;
-    void writeToFile(const std::string& path, const std::vector<int>& v) const;
+    void writeToFile(const std::string& path,
+        const std::vector<int>& original,
+        const std::vector<int>& sorted) const;
     void printVector(const std::vector<int>& v) const;
-
-    // Вспомогательный генератор случайных чисел собственной реализации
     std::vector<int> generateRandomData(int n) const;
 };
